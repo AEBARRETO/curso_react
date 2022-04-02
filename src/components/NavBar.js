618ea5e736c23d306442faff6ce37c6687561f58
EduardoBarreto <ebarreto@dymconsultores.com.ar>
2022-04-02 12:40:57 -0300

    nombre tablet

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,11 +12,11 @@ const NavBar = () =>{
     <Navbar.Brand href="/">Barreto´s Store</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav  >
-        <Nav.Link href="/">Inicio</Nav.Link>
+      <Nav  className="mr-auto">
+        <Nav.Link href="/" className={isActive =>"nav-link" + (!isActive ? " unselected" : "")} >Inicio</Nav.Link>
         <Nav.Link href="/categoria/Notebook">Notebook</Nav.Link>
         <Nav.Link href="/categoria/Celular">Celulares</Nav.Link>
-        <Nav.Link href="/categoria/Table">Tablets</Nav.Link>
+        <Nav.Link href="/categoria/Tablet">Tablets</Nav.Link>
       </Nav>
       <CartWidget/>
     </Navbar.Collapse>
